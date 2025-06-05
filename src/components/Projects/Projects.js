da059dee310ac92175b1d77b22a548ae331606db
Yahya <yehyalegrini@gmail.com>
2025-06-05 19:03:09 +0200

    add projects creativa and attendEase

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,6 +4,9 @@ import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import Balsam from "../../Assets/Projects/Balsam.jpg";
 import github from "../../Assets/Projects/github.jpg";
+import AttendEase from '../../Assets/Projects/AttendEase.png'
+import Creativa from '../../Assets/Projects/CreativaDemo.png'
+
 
 function Projects() {
   return (
@@ -24,6 +27,25 @@ function Projects() {
               title="Balsam"
               description="application mobile developed with react native ,firebase ,it connect patient with professionals and Clinic to in home health care services."
               ghLink="https://github.com/yahyalegrini24/Balsam-"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={AttendEase}
+              isBlog={false}
+              title="AttendEase"
+              description="Web and mobile application developed with React, React Native, and Supabase. It allows university teachers to view daily sessions, mark student attendance easily, add notes, and export attendance records to Excel with a final summary."
+              ghLink="https://github.com/yahyalegrini24/AttendEase"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={Creativa}
+              isBlog={false}
+              title="Creativa Club"
+              description="Website developed with React and Tailwind CSS for the Creativa Club, focused on cinema, photography, and design. It showcases social media acounts, About , and Club members, with an integrated 3D AI chat model that answers all questions about the club."
+              ghLink="https://github.com/yahyalegrini24/Creativa_res"
+              demoLink="https://creativa-umber.vercel.app/"
             />
           </Col>
 
